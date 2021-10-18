b29ff52bc6b5414d2f311f79fc63b9b77708ee75
Tomoya Kuroda <tomoya@impactnorth.com>
2021-10-18 09:05:55 -0400

    5

---
+++ 5.longest-palindromic-substring.js
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode id=5 lang=javascript
+ *
+ * [5] Longest Palindromic Substring
+ *
+ * https://leetcode.com/problems/longest-palindromic-substring/description/
+ *
+ * algorithms
+ * Medium (31.31%)
+ * Likes:    13859
+ * Dislikes: 819
+ * Total Accepted:    1.5M
+ * Total Submissions: 4.8M
+ * Testcase Example:  '"babad"'
+ *
+ * Given a string s, return the longest palindromic substring in s.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: s = "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: s = "cbbd"
+ * Output: "bb"
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: s = "a"
+ * Output: "a"
+ *
+ *
+ * Example 4:
+ *
+ *
+ * Input: s = "ac"
+ * Output: "a"
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= s.length <= 1000
+ * s consist of only digits and English letters.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+  if (s.length < 2) return s;
+  let maxLength = 1;
+  let start = 0;
+  function palindrome(left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      if (right - left + 1 > maxLength) {
+        maxLength = right - left + 1;
+        start = left;
+      }
+      left--;
+      right++;
+    }
+  }
+  for (let i = 0; i < s.length; i++) {
+    palindrome(i - 1, i + 1);
+    palindrome(i, i + 1);
+  }
+  return s.slice(start, start + maxLength);
+};
+
+// @lc code=end
